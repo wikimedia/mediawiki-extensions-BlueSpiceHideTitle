59e879a316c31c570f11871e0f45a7a3f97bdead
hslater <hamish.slater@hallowelt.com>
2025-02-10 13:20:14 +0100

    Update ExtensionRegistry namespace
    ERM38444
    
    [REL1_43, master]

--- src/CssSelectorModuleFactory.php
+++ src/CssSelectorModuleFactory.php
@@ -2,8 +2,8 @@
 
 namespace BlueSpice\HideTitle;
 
-use ExtensionRegistry;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Registration\ExtensionRegistry;
 use MediaWiki\ResourceLoader\Context as ResourceLoaderContext;
 use Wikimedia\ObjectFactory\ObjectFactory;
 
