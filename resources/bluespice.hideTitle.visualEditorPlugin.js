54ea40227eddfc134f3e778aea641ef29ab5060c
hslater <hamish.slater@hallowelt.com>
2025-05-06 19:15:27 +1200

    eslint
    ERM20742
    
    [REL1_43, REL1_43-5.0.x, master]
    
    Change-Id: I94139cba001ebe34f8568583208b532d2c8f42b6

--- resources/bluespice.hideTitle.visualEditorPlugin.js
+++ resources/bluespice.hideTitle.visualEditorPlugin.js
@@ -1,6 +1,4 @@
 bs.vec.registerComponentPlugin(
 	bs.vec.components.META_DIALOG,
-	function( component ) {
-		return new bs.hidetitle.ui.plugin.MWMetaDialog( component );
-	}
+	( component ) => new bs.hidetitle.ui.plugin.MWMetaDialog( component )
 );
